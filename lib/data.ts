1c9f164500983923da2729a3c92eec0f93b51d6f
TuckerHoneycutt <tuckerpersonalmail@gmail.com>
2024-05-02 07:42:01 -0500

    Added revised resume and fixed order of experience

--- lib/data.ts
+++ lib/data.ts
@@ -39,6 +39,54 @@ export const links = [
 
 export const experiencesData = [
 {
+  title: "Parachute | Web Development Fellowship Intern",
+  location: "Remote",
+  description:
+    "Redesigned website and database, boosting UX and performance. Developed secure APIs for AI data ingestion. Implemented Agile methodology, fostering collaboration and productivity. Collaborated with cross-functional teams to align projects with company goals.",
+  icon: React.createElement(CgWorkAlt),
+  date: "January - Present 2024",
+},
+{
+  title: "Verizon Wireless | Retail Specalist",
+  location: "Tuscaloosa, Al",
+  description:
+    "Responsible for cell phone and electronic sales, troubleshooting customer devices, and resolving technical issues.",
+  icon: React.createElement(CgWorkAlt),
+  date: "February - June 2023",
+},
+{
+  title: "University Of Alabama",
+  location: "Tuscaloosa, Al",
+  description:
+    "Majored in Electrical engineering. Changed my major to Computer science in the fall of 2023",
+  icon: React.createElement(LuGraduationCap),
+  date: "August 2019 - Present",
+},
+{
+  title: "Publix | Deli associate",
+  location: "Tuscaloosa, Al",
+  description:
+    "Deli associate in charge of making deli foods, slicing meat, making sandwiches, and stocking shelves.",
+  icon: React.createElement(CgWorkAlt),
+  date: "January 2022 - January 2023",
+},
+{
+  title: "University Of Alabama Antenna And Microwave Laboratory Intern",
+  location: "Tuscaloosa, Al",
+  description:
+    "Designed, developed and tested Arduino circuitry used as the data acquisition system which flies on a drone as part of a wireless power transfer project for the Naval Research Laboratory.  Wrote and debugged the software which reports various voltages in the charging circuit.",
+  icon: React.createElement(CgWorkAlt),
+  date: "May - August 2019",
+},
+{
+  title: "Bestbuy | Seasonal Sales Associate",
+  location: "Tuscaloosa, Al",
+  description:
+    "Computing sales associate at BestBuy. Specialized in helping customers find the best product to fit their computing needs. Assisted in Car Audio, Home Theater, and Home Appliances.",
+  icon: React.createElement(CgWorkAlt),
+  date: "October 2020 - January 2021",
+},
+{
   title: "NASA MSFC | Summer Intern",
   location: "Huntsville, Al",
   description:
@@ -53,46 +101,6 @@ export const experiencesData = [
     "Trained and managed new shipping department workers.  Packaged and shipped HAM radio and computer",
   icon: React.createElement(CgWorkAlt),
   date: "January - May 2019",
-},
-{
-  title: "University Of Alabama Antenna And Microwave Laboratory Intern",
-  location: "Tuscaloosa, Al",
-  description:
-    "Designed, developed and tested Arduino circuitry used as the data acquisition system which flies on a drone as part of a wireless power transfer project for the Naval Research Laboratory.  Wrote and debugged the software which reports various voltages in the charging circuit.",
-  icon: React.createElement(CgWorkAlt),
-  date: "May - August 2019",
-},
-{
-  title: "University Of Alabama",
-  location: "Tuscaloosa, Al",
-  description:
-    "Majored in Electrical engineering. Changed my major to Computer science in the fall of 2023",
-  icon: React.createElement(LuGraduationCap),
-  date: "August 2019 - Present",
-},
-{
-  title: "Bestbuy | Seasonal Sales Associate",
-  location: "Tuscaloosa, Al",
-  description:
-    "Computing sales associate at BestBuy. Specialized in helping customers find the best product to fit their computing needs. Assisted in Car Audio, Home Theater, and Home Appliances.",
-  icon: React.createElement(CgWorkAlt),
-  date: "October 2020 - January 2021",
-},
-{
-  title: "Publix | Deli associate",
-  location: "Tuscaloosa, Al",
-  description:
-    "Deli associate in charge of making deli foods, slicing meat, making sandwiches, and stocking shelves.",
-  icon: React.createElement(CgWorkAlt),
-  date: "January 2022 - January 2023",
-},
-{
-  title: "Verizon Wireless | Retail Specalist",
-  location: "Tuscaloosa, Al",
-  description:
-    "Responsible for cell phone and electronic sales, troubleshooting customer devices, and resolving technical issues.",
-  icon: React.createElement(CgWorkAlt),
-  date: "February - June 2023",
 },
 ] as const;
 
